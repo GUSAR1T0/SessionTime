090d036fd87dbe11fc360bfab7c638751c48ce94
Roman Mashenkin <vl_vasilevsky@mail.ru>
2016-03-27 03:16:18 +0300

    Updated project (ic_launchers, background, MenuState.java)
    Small changes: added icons launcher for app, changed picture of Menu
    Background and code of MenuState class.

--- core/src/com/teamproject/game/states/MenuState.java
+++ core/src/com/teamproject/game/states/MenuState.java
@@ -14,20 +14,34 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
  */
 public class MenuState extends State {
 
-    private static final float SCALING_FACTOR = (float) Gdx.graphics.getHeight() / 2160 ;
+    private static final float SCALING_FACTOR = (float) Gdx.graphics.getHeight() / 2880 ;
+    private static final String MENU_BACKGROUND = "menu_background_2560x1600.png";
 
     private Texture background;
-    private Sprite button1;
+    private Sprite button1, button2, button3;
 
     public MenuState(GameStateManager gsm) {
         super(gsm);
         camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-        background = new Texture(Gdx.files.internal("main_background.png"));
+        background = new Texture(Gdx.files.internal(MENU_BACKGROUND));
+
         button1 = new Sprite(new Texture(Gdx.files.internal("icon_cat.png")));
-        button1.setPosition(camera.position.x - button1.getWidth() / 2,
-                camera.position.y - button1.getHeight() / 2);
+        button1.setPosition(camera.position.x - button1.getWidth() / 2 -
+                1.5f * SCALING_FACTOR * button1.getWidth(),
+                camera.position.y - button1.getWidth() / 2);
         button1.setScale(SCALING_FACTOR);
+
+        button2 = new Sprite(new Texture(Gdx.files.internal("icon_cat.png")));
+        button2.setPosition(camera.position.x - button2.getWidth() / 2,
+                camera.position.y - button2.getHeight() / 2);
+        button2.setScale(SCALING_FACTOR);
+
+        button3 = new Sprite(new Texture(Gdx.files.internal("icon_cat.png")));
+        button3.setPosition(camera.position.x - button3.getWidth() / 2 +
+                1.5f * SCALING_FACTOR * button3.getWidth(),
+                camera.position.y - button3.getHeight() / 2);
+        button3.setScale(SCALING_FACTOR);
     }
 
     @Override
@@ -46,6 +60,8 @@ public class MenuState extends State {
         sb.begin();
         sb.draw(background, 0, 0);
         button1.draw(sb);
+        button2.draw(sb);
+        button3.draw(sb);
         sb.end();
     }
 
