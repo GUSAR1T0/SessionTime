58d3a5c4ff60dbaed6fa107499adc461c5cc3363
Ganz <smirnov_ig@mail.ru>
2016-03-28 20:53:13 +0300

    Important changes in core-part of code
    Updated code in starters of game, rewritten MenuScreen (half of
    realization), changed class name from "SessionTime" to "STGame"

--- core/src/com/teamproject/game/additions/Constant.java
+++ core/src/com/teamproject/game/additions/Constant.java
@@ -13,11 +13,18 @@ public class Constant {
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 720;
 
+    //Width and height of World
+    public static final int WORLD_WIDTH = 1000;
+    public static final int WORLD_HEIGHT = 1000;
+
     //Title of game
     public static final String TITLE = "Session Time";
 
     //Scaling factor for Android devices
     public static final float SCALING_FACTOR = (float) Gdx.graphics.getHeight() / 2880 ;
+
+    //The ratio of height and width
+    public static final float RATIO = (float) Gdx.graphics.getHeight() / Gdx.graphics.getWidth();
 
     //Setting of sequence of English and Russian letters, numerals and special chars
     public static final String FONT_CHARS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" +
--- core/src/com/teamproject/game/screens/MenuScreen.java
+++ core/src/com/teamproject/game/screens/MenuScreen.java
@@ -1,9 +1,13 @@
 package com.teamproject.game.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.teamproject.game.STGame;
 import com.teamproject.game.additions.Constant;
 
 /**
@@ -11,67 +15,80 @@ import com.teamproject.game.additions.Constant;
  *
  * This class is view of game menu:
  * - setting parameters of orthographic camera;
- * - drawing background and icons of buttons.
+ * - drawing background and images for buttons.
  */
-public class MenuScreen extends Screen {
+public class MenuScreen implements Screen {
 
     private Texture background;
-    private Sprite button1, button2, button3, logo;
+    private Sprite logo;
+    private Stage stage;
+    private final STGame game;
 
-    public MenuScreen(GameScreenManager gsm) {
-        super(gsm);
-        camera.setToOrtho(false);
+    float textureScale;
 
+    public MenuScreen(STGame game) {
+
+        this.game = game;
+
+        stage = new Stage(new StretchViewport(Constant.WORLD_WIDTH,
+                Constant.WORLD_HEIGHT * Constant.RATIO));
+        Gdx.input.setInputProcessor(stage);
+
+        createMenuScreen();
+    }
+
+    private void createMenuScreen() {
+        //Setting background texture
         background = new Texture(Gdx.files.internal(Constant.MENU_BACKGROUND));
 
-        button1 = new Sprite(new Texture(Gdx.files.internal("icon_cat.png")));
-        button1.setPosition(camera.position.x - button1.getWidth() / 2 -
-                1.5f * Constant.SCALING_FACTOR * button1.getWidth(),
-                camera.position.y - button1.getWidth() / 2);
-        button1.setScale(Constant.SCALING_FACTOR);
-
-        button2 = new Sprite(new Texture(Gdx.files.internal("icon_cat.png")));
-        button2.setPosition(camera.position.x - button2.getWidth() / 2,
-                camera.position.y - button2.getHeight() / 2);
-        button2.setScale(Constant.SCALING_FACTOR);
-
-        button3 = new Sprite(new Texture(Gdx.files.internal("icon_cat.png")));
-        button3.setPosition(camera.position.x - button3.getWidth() / 2 +
-                1.5f * Constant.SCALING_FACTOR * button3.getWidth(),
-                camera.position.y - button3.getHeight() / 2);
-        button3.setScale(Constant.SCALING_FACTOR);
-
+        //Setting logo texture
         logo = new Sprite(new Texture(Gdx.files.internal("logo.png")));
-        logo.setPosition(camera.position.x - logo.getWidth() / 2,
-                Gdx.graphics.getHeight()* 3/4);
-        logo.setScale(2 * Constant.SCALING_FACTOR);
+        logo.setSize(logo.getWidth() * 2 * Constant.SCALING_FACTOR,
+                logo.getHeight() * 2 * Constant.SCALING_FACTOR);
+        logo.setPosition(Gdx.graphics.getWidth() / 2 - logo.getWidth() / 2,
+                Gdx.graphics.getHeight() * 6 / 7 - logo.getHeight() / 2);
+    }
+
+    public float getTextureScale() { return stage.getWidth() / Gdx.graphics.getWidth(); }
+
+    @Override
+    public void show() {
+
     }
 
     @Override
-    protected void handleInput() {
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.getBatch().begin();
+        stage.getBatch().draw(background, 0, 0);
+        logo.draw(stage.getBatch());
+        stage.getBatch().end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
 
     }
 
     @Override
-    public void update(float dt) {
-        handleInput();
+    public void pause() {
+
     }
 
     @Override
-    public void render(SpriteBatch sb) {
-        //Drawing batch
-        sb.setProjectionMatrix(camera.combined);
-        sb.begin();
-        sb.draw(background, 0, 0);
-        button1.draw(sb);
-        button2.draw(sb);
-        button3.draw(sb);
-        logo.draw(sb);
-        sb.end();
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
     }
 
     @Override
     public void dispose() {
         background.dispose();
+        stage.dispose();
     }
 }
