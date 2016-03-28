006951218a3d30b7fe08363b7d5d87e3b3eb0958
Roman Mashenkin <vl_vasilevsky@mail.ru>
2016-03-29 00:54:35 +0300

    Undo the last changes

--- core/src/com/teamproject/game/screens/MenuScreen.java
+++ core/src/com/teamproject/game/screens/MenuScreen.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-<<<<<<< HEAD
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -14,34 +13,23 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
-=======
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
->>>>>>> 58d3a5c4ff60dbaed6fa107499adc461c5cc3363
 import com.teamproject.game.STGame;
 import com.teamproject.game.additions.Constant;
 
 /**
  * Created by Roman_Mashenkin on 26.03.2016.
  *
-<<<<<<< HEAD
  * This class is view of game menu.
  * The main operations:
  * - setting stage;
  * - creating table and adding buttons;
  * - drawing background, images for logo and buttons.
-=======
- * This class is view of game menu:
- * - setting parameters of orthographic camera;
- * - drawing background and images for buttons.
->>>>>>> 58d3a5c4ff60dbaed6fa107499adc461c5cc3363
  */
 public class MenuScreen implements Screen {
 
     private Texture background;
     private Sprite logo;
     private Stage stage;
-<<<<<<< HEAD
     private ImageButton playButton, settingButton, menuButton;
     private Table table;
     private final STGame game;
@@ -60,34 +48,12 @@ public class MenuScreen implements Screen {
         //Setting background texture
         background = new Texture(Gdx.files.internal(Constant.MENU_BACKGROUND));
 
-=======
-    private final STGame game;
-
-    float textureScale;
-
-    public MenuScreen(STGame game) {
-
-        this.game = game;
-
-        stage = new Stage(new StretchViewport(Constant.WORLD_WIDTH,
-                Constant.WORLD_HEIGHT * Constant.RATIO));
-        Gdx.input.setInputProcessor(stage);
-
-        createMenuScreen();
-    }
-
-    private void createMenuScreen() {
-        //Setting background texture
-        background = new Texture(Gdx.files.internal(Constant.MENU_BACKGROUND));
-
->>>>>>> 58d3a5c4ff60dbaed6fa107499adc461c5cc3363
         //Setting logo texture
         logo = new Sprite(new Texture(Gdx.files.internal("logo.png")));
         logo.setSize(logo.getWidth() * 2 * Constant.SCALING_FACTOR,
                 logo.getHeight() * 2 * Constant.SCALING_FACTOR);
         logo.setPosition(Gdx.graphics.getWidth() / 2 - logo.getWidth() / 2,
                 Gdx.graphics.getHeight() * 6 / 7 - logo.getHeight() / 2);
-<<<<<<< HEAD
 
         //Setting menu buttons
         playButton = makeButton("play_button");
@@ -167,33 +133,6 @@ public class MenuScreen implements Screen {
     }
 
     @Override
-=======
-    }
-
-    public float getTextureScale() { return stage.getWidth() / Gdx.graphics.getWidth(); }
-
-    @Override
-    public void show() {
-
-    }
-
-    @Override
-    public void render(float delta) {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        stage.getBatch().begin();
-        stage.getBatch().draw(background, 0, 0);
-        logo.draw(stage.getBatch());
-        stage.getBatch().end();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
->>>>>>> 58d3a5c4ff60dbaed6fa107499adc461c5cc3363
     public void pause() {
 
     }
@@ -212,9 +151,6 @@ public class MenuScreen implements Screen {
     public void dispose() {
         background.dispose();
         stage.dispose();
-<<<<<<< HEAD
         game.dispose();
-=======
->>>>>>> 58d3a5c4ff60dbaed6fa107499adc461c5cc3363
     }
 }
